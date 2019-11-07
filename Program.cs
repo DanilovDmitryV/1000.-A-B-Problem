56700d4ea716b3b6f1e11fa5851e52130421ad85
Данилов Дмитрий Валерьевич <DanilovDV@rusgeology.ru>
2019-11-07 14:11:00 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1000.A_B_Problem
+{
+    public class Sum
+    {
+        private static void Main()
+        {
+            string[] numbers = Console.ReadLine().Split(' ');
+            Console.WriteLine(int.Parse(numbers[0]) + int.Parse(numbers[1]));
+        }
+    }
+}
